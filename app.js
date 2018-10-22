db75c074829f06f65c2595344652c25f10182183
elena churilov <elena.churilov@gmail.com>
2018-10-21 22:53:54 -0700

    Add User Registration

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const db = require('./config/keys').mongoURI;
 mongoose
@@ -9,5 +10,13 @@ mongoose
 
 const app = express();
 const port = process.env.PORT || 5000;
+const users = require("./routes/api/users");
+const events = require("./routes/api/events");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/events", events);
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Get carried away!"));
