2303d83fed090a04c97cfe1ab11aa951d47cdbd9
elena churilov <elena.churilov@gmail.com>
2018-10-23 16:08:27 -0700

    Update Bookmark validations

--- validations/bookmark.js
+++ validations/bookmark.js
@@ -4,7 +4,12 @@ const isEmpty = require("./is-empty");
 module.exports = function validateNewBookmark(data) {
   let errors = {};
 
-  data.password = !isEmpty(data.password) ? data.password : "";
+  data.user_id = !isEmpty(data.user_id) ? data.user_id : "";
+  data.query = !isEmpty(data.query) ? data.query : "";
+
+  if (Validator.isEmpty(data.user_id)) {
+    errors.user_id = "User field is required";
+  }
 
   if (Validator.isEmpty(data.query)) {
     errors.query = "Query field is required";
