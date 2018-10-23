3033ad7a28661c4a352a44acf4e11857206cfeb8
Sam McClure <samkmcclure@gmail.com>
2018-10-23 12:58:44 -0700

    Add youtube search and news search

--- routes/api/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  imgurId: "76bbabc235c10ff",
-  giphyId: "XP9fekPCdRZvOyE3IABbbjFf5RPyYUpa"
-};
--- routes/api/search.js
+++ routes/api/search.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const keys = require("./keys");
+const keys = require("./api_keys");
+const NewsAPI = require("newsapi");
+const news = new NewsAPI(keys.newsId);
+const youtubeSearch = require("youtube-api-v3-search");
+
+//https://stackoverflow.com/questions/16686640/function-to-get-yesterdays-date-in-javascript-in-format-dd-mm-yyyy
+let today = new Date();
+let yesterday = new Date(today);
+yesterday.setDate(today.getDate() - 1);
+const dd = yesterday.getDate();
+const mm = yesterday.getMonth() + 1; //January is 0!
+const yyyy = yesterday.getFullYear();
+if (dd < 10) {
+  dd = "0" + dd;
+}
+if (mm < 10) {
+  mm = "0" + mm;
+}
+yesterday = yyyy + "-" + mm + "-" + dd;
 
 router.get("/:searchQuery", (req, res) => {
   const processedQuery = req.params.searchQuery.split(" ").join("+");
-  debugger;
+
   const imgurCallback = () => {
     return axios({
       method: "get",
@@ -35,12 +53,57 @@ router.get("/:searchQuery", (req, res) => {
       });
   };
 
-  Promise.all([imgurCallback(), giphyCallback()])
+  const newsCallback = () => {
+
+    return news.v2
+      .everything({
+        q: processedQuery,
+        language: "en",
+        sortBy: "relevancy",
+        from: yesterday
+      })
+      .then(res => {
+        return { data: res.articles };
+      })
+      .catch(err => {
+        return err;
+      });
+  };
+
+  const youtubeCallback = () => {
+    const options = {
+      q: processedQuery,
+      part: 'snippet',
+      type: 'video',
+      maxResults: 10,
+      order: 'viewCount',
+      publishedAfter: yesterday + "T00:00:00Z",
+      safeSearch: "strict",
+      videoEmbeddable: "true"
+    };
+    
+    return youtubeSearch(keys.youtubeId, options)
+      .then(res => {
+        return { data: res.items };
+      })
+      .catch(err => {
+        return err;
+      });
+  };
+
+  Promise.all([
+    imgurCallback(),
+    giphyCallback(),
+    newsCallback(),
+    youtubeCallback()
+  ])
     .then(function(value) {
       const allTheData = {};
       allTheData["imgur"] = value[0].data.slice(0, 10);
       allTheData["giphy"] = value[1].data.slice(0, 10);
-      console.log(allTheData);
+      allTheData["news"] = value[2].data.slice(0, 10);
+      allTheData["youtube"] = value[3].data;
+      console.log(allTheData)
       return allTheData;
     })
     .catch(err => {
