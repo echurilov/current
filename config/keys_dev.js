ca966e0e45fcafe0c21f86482ec1d1226dfb347b
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-10-23 11:49:51 -0700

    Change keys

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://current-user:z8guhny8fPr8ViN@ds239703.mlab.com:39703/whats-current",
+  mongoURI: "mongodb://current-user:z8guhny8fPr8ViN@ds239703.mlab.com:39703/whats-current-hi",
   secretOrKey: "secret"
 };
