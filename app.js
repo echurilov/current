4d642c22b1f2d47549e286f5f7b80cc21bfb0dbc
elena churilov <elena.churilov@gmail.com>
2018-10-23 15:40:37 -0700

    Add Bookmarks model + routes to app

--- app.js
+++ app.js
@@ -16,10 +16,12 @@ mongoose
 const app = express();
 const port = process.env.PORT || 5000;
 const users = require("./routes/api/users");
+const bookmarks = require("./routes/api/bookmarks");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api/users", users);
+app.use("/api/bookmarks", bookmarks);
 app.use(passport.initialize());
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
