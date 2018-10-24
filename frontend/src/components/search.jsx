f758c65aff4d0e0e051c810a980c0495a59dc3da
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-10-24 16:52:28 -0700

    Finish scrolling google trends

--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import '../css/search.css'
 // import { connect } from 'react-redux';
-import SearchResults from './search_results';
+// import SearchResults from './search_results';
 
 class Search extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { searchTerm: '', render: false, dailyTrends: [], dailyTrends2: [] };
+        this.state = { searchTerm: '', render: false };
         this.submitSearch = this.submitSearch.bind(this);
     }
 
-    componentDidMount() {
-        this.setState({ dailyTrends: ['Ariana Grande', 'Westminster Dog Show', 'World Cup', 'Amazon Go', 'Halloween', 'Game of Thrones'] })
-        this.setState({ dailyTrends2: ['Matt Damon', 'The Warriors', 'Voter Registration', 'Supreme Court', 'Haunted Houses', 'Drake'] })
-        this.setState({ render: true })
-    }
 
     submitSearch(searchTermInput) {
         let searchTerm = searchTermInput || document.getElementById('search-input').value;
@@ -23,25 +18,31 @@ class Search extends React.Component {
     }
 
     render() {
-        let { render, dailyTrends, dailyTrends2 } = this.state;
+        let { render } = this.state;
 
-        if (!render) {
-            return null;
-        }
+        let dailyTrends = ['Ariana Grande', 'Westminster Dog Show', 'World Cup', 'Piperade']
+        let dailyTrends2 = ['Matt Damon', 'The Warriors', 'Voter Registration', 'Supreme Court']
+        let dailyTrends3 = ['Amazon Go', 'Halloween', 'Game of Thrones', 'Haunted Houses', 'Drake']
 
         let trendButtons = [];
         for(let i = 0; i < dailyTrends.length; i++) {
-            let btn = <button className="trend-btn animated slideInLeft" onClick={this.submitSearch(dailyTrends[i])}> {dailyTrends[i]} </button>
+            let btn = <button className="trend-btn" onClick={this.submitSearch(dailyTrends[i])}> {dailyTrends[i]} </button>
             trendButtons.push(btn);
         }
 
         let trendButtons2 = [];
         for(let i = 0; i < dailyTrends2.length; i++) {
-            let btn = <button className="trend-btn animated slideInLeft" onClick={this.submitSearch(dailyTrends2[i])}> {dailyTrends2[i]} </button>
+            let btn = <button className="trend-btn" onClick={this.submitSearch(dailyTrends2[i])}> {dailyTrends2[i]} </button>
             trendButtons2.push(btn);
         }
 
-        let results = ''
+        let trendButtons3 = [];
+        for(let i = 0; i < dailyTrends3.length; i++) {
+            let btn = <button className="trend-btn" onClick={this.submitSearch(dailyTrends3[i])}> {dailyTrends3[i]} </button>
+            trendButtons3.push(btn);
+        }
+
+        let results = 'result'
         // let results = <SearchResults searchTerm={searchTerm} />
 
         return (
@@ -62,7 +63,15 @@ class Search extends React.Component {
                 </div>
                 
                 <div className="trends">
-                    {trendButtons}
+                    <div className="item-1">
+                        {trendButtons}
+                    </div>
+                    <div className="item-2">
+                        {trendButtons2}
+                    </div>
+                    <div className="item-3">
+                        {trendButtons3}
+                    </div>
                 </div>
 
                 {(render) ? results : null}
