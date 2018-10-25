1d5fe04c90689aa8c9695326384f2151123aee55
elena churilov <elena.churilov@gmail.com>
2018-10-25 12:08:11 -0700

    Fix Bookmarks reducer path

--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,19 +1,19 @@
 import { combineReducers } from 'redux';
+import bookmarksReducer from './bookmarks_reducer';
 // import imgurReducer from './api_results/imgur_reducer';
 import giphyReducer from './api_results/giphy_reducer';
 import trendsReducer from './api_results/trends_reducer';
 import newsReducer from './api_results/news_reducer';
 import youtubeReducer from './api_results/youtube_reducer';
 import relatedTopicsReducer from './api_results/related_topics_reducer';
-import bookmarksReducer from './api_results/bookmarks_reducer';
 // import users from './users_reducer';
 
 export default combineReducers({
+  bookmarks: bookmarksReducer,
   // imgur: imgurReducer,
   giphy: giphyReducer,
   trends: trendsReducer,
   news: newsReducer,
   youtube: youtubeReducer,
   relatedTopics: relatedTopicsReducer,
-  bookmarks: bookmarksReducer
 });
