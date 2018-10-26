7f15d1b10c69af5e14b80034f283539c9eadf94e
Wade Coufal <wade.coufal@gmail.com>
2018-10-25 17:41:06 -0700

    Fix imgur title positioning

--- frontend/src/components/results/imgur_index_item.jsx
+++ frontend/src/components/results/imgur_index_item.jsx
@@ -10,8 +10,8 @@ class ImgurIndexItem extends React.Component {
                className="imgur-logo" />
         </div>
         <img className="imgur-image" src={this.props.imgurItem.link} alt="imgur" />
-        <div>
-          <h1 className="imgur-header-title">{this.props.imgurItem.title}</h1>
+        <div className="imgur-title-container">
+          <h1 className="imgur-title">{this.props.imgurItem.title}</h1>
         </div>
       </div>;
   }
