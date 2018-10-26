8bc26e97ca0bedc6b803ce19adce9ca264a23500
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-10-26 10:32:35 -0700

    Update bookmarks reducer

--- frontend/src/reducers/bookmarks_reducer.js
+++ frontend/src/reducers/bookmarks_reducer.js
@@ -3,6 +3,7 @@ import {
   RECEIVE_BOOKMARKS,
   REMOVE_BOOKMARK,
 } from "../actions/bookmark_actions";
+import merge from 'lodash';
 
 const _nullBookmark = Object.freeze({
   id: null
@@ -12,8 +13,9 @@ const bookmarkReducer = (state = _nullBookmark, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_BOOKMARK:
-      debugger
-      return { [action.bookmark.data.bookmark._id]:action.bookmark.data.bookmark };
+      // debugger
+      let newState = merge({}, state, { [action.bookmark.data.bookmark._id]: action.bookmark.data.bookmark });
+      return newState;
     case RECEIVE_BOOKMARKS:
       // debugger
       return action.payload;
