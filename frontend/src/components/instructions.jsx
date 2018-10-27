fb29dffd000f80a2bb03a2eccfd833bd98ef35f0
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-10-27 14:08:52 -0700

    Add new demo user

--- frontend/src/components/instructions.jsx
+++ frontend/src/components/instructions.jsx
@@ -8,12 +8,14 @@ class Instructions extends React.Component {
         <p>
           welcome to Current! 
         </p>
+        <br></br>
         <p>
           we are a trending content aggregator. the words in the scrolling bar are today's
           trending topics - click on them to see more! you can search for just about anything in the search bar
           and see the latest content about it from multiple news sites, youtube, imgur, and even
           giphy. 
         </p>
+        <br></br>
         <p>
           if you search something you like, save it by clicking the + button, and search 
           it again easily from your bookmarks! and you can hit the home button to go back to 
