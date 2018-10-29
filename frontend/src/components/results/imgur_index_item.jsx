072b2972add816cde5fea5c283f1e14942fb5ea0
Wade Coufal <wade.coufal@gmail.com>
2018-10-28 20:35:12 -0700

    Minor imgur styling

--- frontend/src/components/results/imgur_index_item.jsx
+++ frontend/src/components/results/imgur_index_item.jsx
@@ -20,7 +20,9 @@ class ImgurIndexItem extends React.Component {
       rel="noopener noreferrer" 
       href={link} 
       className="imgur-index-item">
-        <img className="imgur-image" src={imgurItem.link} alt="imgur" />
+        <div className="imgur-image-container">
+          <img className="imgur-image" src={imgurItem.link} alt="imgur" />
+        </div>
         <div className="imgur-title-container">
           <h1 className="imgur-title">{imgurTitle}</h1>
           <img src="https://i.imgur.com/cX0VsCp.png" alt="imgur-logo"
