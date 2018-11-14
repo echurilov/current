d1aceee000521f4be2b592e085f42a7dd951881e
Sam McClure <samkmcclure@gmail.com>
2018-11-14 13:23:32 -0800

    Add backend method to fetch trends directly from google trends website

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import 'normalize.css';
 import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
 import * as APIUtil from './util/session_api_util';
+import { fetchTrends } from './actions/trends_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
@@ -23,4 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     ReactDOM.render(<Root store={store} />, root);
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.fetchTrends = fetchTrends;
 })
--- routes/api/trends.js
+++ routes/api/trends.js
@@ -1,22 +1,45 @@
 const express = require("express");
 const router = express.Router();
-const googleTrends = require("google-trends-api-es");
+// const googleTrends = require("google-trends-api-es");
 const googleTrends2 = require("google-trends-api");
+const axios = require("axios");
+var parseString = require("xml2js").parseString;
 
 router.get("/test", (req, res) =>
   res.json({ msg: "This is the google trends route" })
 );
 
 router.get("/", (req, res) => {
-  return googleTrends
-    .hotTrends("US")
-    .then(function (results) {
-      res.json({
-        trends: results
+  // return googleTrends
+  //   .hotTrends("US")
+  //   .then(function (results) {
+  //     res.json({
+  //       trends: results
+  //     });
+  //   })
+  //   .catch(function (err) {
+  //     console.error("Oh no there was an error", err);
+  //   });
+  return axios({
+    method: "get",
+    url: `https://trends.google.com/trends/trendingsearches/daily/rss?geo=US`,
+  })
+    .then(res => {
+      let items = [];
+      parseString(res.data, function (err, result) {
+        items = result.rss.channel[0].item;
+      });  
+
+      let trends = [];
+      items.forEach((item) => {
+        trends.push(item.title[0]);
       });
+
+      console.log(trends);
+      return res.json({trends: trends});
     })
-    .catch(function (err) {
-      console.error("Oh no there was an error", err);
+    .catch(err => {
+      return err;
     });
 });
 
